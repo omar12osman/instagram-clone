ca63ef8c670d3eb3eabb7617ee6208fdabee4f93
Majid Osman <omar12osman12@gmail.com>
2018-11-20 18:43:55 +0100

    Initial Commit

---
+++ instagram clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram clone
+//
+//  Created by Majid Osman on 2018-11-20.
+//  Copyright © 2018 Majid Osman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
